355a28dda766a6fb51564f0e7cc6d9047dd03f37
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-29 14:53:04 +0100

    added props for nextGA component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
 
+interface NextGAProps {
+  mId: string
+}
+
 // Delete me
 export const Thing = () => {
   return <div>the snozzberries taste like snozzberries</div>;
